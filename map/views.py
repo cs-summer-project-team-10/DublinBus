f77043ea535ca3adb0f06c95c6c873c6a542d8bc
Stephen Gaffney <stephen.gaffney@ucdconnect.ie>
2019-07-10 12:09:18 +0100

    Latest changes on views.py

--- map/views.py
+++ map/views.py
@@ -67,13 +67,58 @@ def return_routes(request):
 
 
 class TempRoute():
-    def __init__(self, line_id, route_id, number_stops):
+    def __init__(self, line_id, route_id, start_stop, dest_stop):
         self.line_id = line_id
         self.route_id = route_id
+        self.start_stop = start_stop
+        self.dest_stop = dest_stop
+        #self.all_stops = stop_list
         #self.number_stops = number_stops
+        self.stops_serviced = self.get_stop_list()
+        self.subroute = self.get_subroute()
+        self.number_stops = self.get_number_stops_subroute()
 
     def get_stop_list(self):
-        self.stops_serviced = []
+        stops_serviced = list(RouteStops.objects.values_list('stop_id', 'stop_order').filter(route_id = self.route_id))
+        return stops_serviced
+
+    def print_route(self):
+        print("ROUTE on", self.route_id, self.stops_serviced)
+
+    def print_subroute(self):
+        print("SUBROUTE on ",self.route_id, self.subroute)
+
+    def print_number_stops(self):
+        print("Number stops ", self.number_stops)
+
+    def print_lineID(self):
+        print("Line ID is ", self.line_id)
+
+    def get_subroute(self):
+        subroute = []
+        for tuple in self.stops_serviced:
+            #print(type(tuple))
+            #print(self.start_stop.stop_id)
+            #print(tuple[0])
+            if tuple[0] == self.start_stop.stop_id:
+                self.start_stop_order = tuple[1]
+
+            elif tuple[0] == self.dest_stop.stop_id:
+                self.dest_stop_order = tuple[1]
+
+        for tuple in self.stops_serviced:
+            stop_order = tuple[1]
+            #print(stop_order)
+            #print(self.dest_stop_order)
+            #print(self.start_stop_order)
+            if stop_order <= self.dest_stop_order and stop_order >= self.start_stop_order:
+                subroute.append(tuple)
+        return subroute
+
+    def get_number_stops_subroute(self):
+        number_stops = len(self.subroute)
+        return number_stops - 1
+
 
 
 def return_routes2(request):
@@ -87,28 +132,31 @@ def return_routes2(request):
     dest_stop = BusStop.objects.get(stop_id = dest_stop)
 
     start_stop_routes_serviced = start_stop.routes_serviced()
+    #Dest stop sometimes returns NaN
     dest_stop_routes_serviced = dest_stop.routes_serviced()
 
-    print("start", start_stop_routes_serviced)
-    print("End", dest_stop_routes_serviced)
+    print("start stop", start_stop)
+    print("dest stop", dest_stop)
+
+    print("start routes serviced", start_stop_routes_serviced)
+    print("dest routes serviced", dest_stop_routes_serviced)
 
     #common_routes = start_stop_routes_serviced.intersection(dest_stop_routes_serviced)
     common_routes = [route for route in start_stop_routes_serviced if route in dest_stop_routes_serviced]
 
-    print("common", common_routes)
+    print("common routes", common_routes)
 
-    stop_list = []
-    for route in common_routes:
-        stops = list(RouteStops.objects.values_list('stop_id', 'stop_order').filter(route_id = route))
-        stop_list += stops
+    list_of_lists = []
+    temp_route_dict = {}
 
-    print(stop_list)
-
-'''
-    common_routes_info_dict = {}
-    for common_route in common_routes:
-        common_routes_info_dict[common_route] = Route(common_route, 11)
-
-    for key in common_routes_info_dict:
-        print("Key",key)
-'''
+    for route_id in common_routes:
+        line_id = route_id.split("_")[0]
+        temp_route_dict[route_id] = TempRoute(line_id, route_id, start_stop, dest_stop)
+        #stop_list = TempRoute_dict[route].stops_serviced
+        #stops = list(RouteStops.objects.values_list('stop_id', 'stop_order').filter(route_id = route))
+        #stop_list += stops
+        #list_of_lists.append(stop_list)
+        temp_route_dict[route_id].print_route()
+        temp_route_dict[route_id].print_subroute()
+        temp_route_dict[route_id].print_lineID()
+        temp_route_dict[route_id].print_number_stops()

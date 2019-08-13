7e7a00d41aa28997c72040f150e81cb0b900d5f0
Stephen Gaffney <stephen.gaffney@ucdconnect.ie>
2019-08-13 16:14:38 +0100

    settings

--- website/settings.py
+++ website/settings.py
@@ -74,17 +74,28 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 #Edit below for own local DB
-DATABASES = {
-       'default': {
-           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': 'dub_bus',
-           'USER': 'student',
-           'PASSWORD': 'group10bus',
-           'HOST': '127.0.0.1',
-           'PORT': '3333',
-       }
-   }
+# DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#            'NAME': 'dub_bus',
+#            'USER': 'student',
+#            'PASSWORD': 'group10bus',
+#            'HOST': '127.0.0.1',
+#            'PORT': '3333',
+#        }
+#    }
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'new_models',
+        'USER': 'stephen',
+        'PASSWORD': '1993',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 
 
 
